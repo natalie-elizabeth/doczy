7be9d0dd31b67d776a13270da11f0cee182aca2a
Natalie <natalie.elizabeth@andela.com>
2017-06-22 15:44:15 +0300

    testing authAction on createUser

--- __tests__/AuthActions.js
+++ __tests__/AuthActions.js
@@ -1,2 +1,12 @@
 import * as actions from '../client/src/actions/authActions';
 import * as c from '../client/src/actions/actionTypes';
+
+describe('actions', () => {
+  it('should create a user', () => {
+    const user = "Natalie Larhette";
+    const expectedAction = {
+      type: c.CREATE_USER, user
+    };
+    expect(actions.createUser(user)).toEqual(expectedAction);
+  });
+});
