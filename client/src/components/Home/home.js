d7b1f548527e429c68c5159d52139665d8f1d6a1
Natalie <natalie.elizabeth@andela.com>
2017-06-27 12:21:32 +0300

    UI homepage changes

--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 import Login from '../User/loginPage';
 import { Card, CardTitle } from 'material-ui/Card';
 import DocumentList from '../Documents/documentList';
-import DocumentContainer from '../Documents/documentContainer';
 
 const Home = () => (
   <div>
     <Card className="container">
-      <center><CardTitle title="Doczy" subtitle="All your documents in one place" style={{}} /></center>
+      <center><CardTitle title="Doczy" subtitle="All your documents in one place" /></center>
     </Card>
     <br /><br />
-    <Card className="container">< DocumentContainer /> </Card>
   </div>
 );
 export default Home;
