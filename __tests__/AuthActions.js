6df162124fb22ac3c4a8eca5d12172f80cacee46
Natalie <natalie.elizabeth@andela.com>
2017-06-28 10:48:34 +0300

    user authentication tests

--- __tests__/AuthActions.js
+++ __tests__/AuthActions.js
@@ -4,6 +4,7 @@ import nock from 'nock';
 import expect from 'expect';
 import * as actions from '../client/src/actions/authActions';
 import * as c from '../client/src/actions/actionTypes';
+import * as endpoint from '../client/src/api/api';
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
@@ -13,6 +14,26 @@ describe('actions', () => {
     const user = "Natalie Larhette";
     const expectedAction = {
       type: c.CREATE_USER, user
+    };
+    expect(actions.createUser(user)).toEqual(expectedAction);
+  });
+  it('should create an action on user log out', () => {
+    const expectedAction = {
+      type: c.LOGOUT,
+    };
+    expect(actions.logoutUser()).toEqual(expectedAction);
+  });
+  it('should create an action to signup a user', () => {
+    const user = {
+      username: 'username',
+      firstname: 'firstname',
+      lastname: "lastname",
+      email: "email",
+      password: "password"
+    };
+    const expectedAction = {
+      type: c.CREATE_USER,
+      user
     };
     expect(actions.createUser(user)).toEqual(expectedAction);
   });
