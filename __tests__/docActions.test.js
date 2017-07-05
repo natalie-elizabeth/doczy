7c43e8700900178ef0de8b1e3738913a43ac495d
Natalie <natalie.elizabeth@andela.com>
2017-07-05 12:02:48 +0300

    frontend tests for document actions

--- __tests__/docActions.test.js
+++ __tests__/docActions.test.js
@@ -1,10 +1,55 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import nock from 'nock';
-import * as actions from '../../client/src/actions/documentsAction';
-import * as c from '../../client/src/actions/actionTypes';
+import * as actions from '../client/src/actions/docActions';
+import * as c from '../client/src/actions/actionTypes';
 
 const middleware = [thunk];
-const mockStore = configureMockStore(middlewares);
+const mockStore = configureMockStore(middleware);
+
+describe('actions', () => {
+  it('should add document on request', () => {
+    const action = {
+      type: c.DOCUMENT_ADD_REQUEST
+    };
+    expect(actions.documentsAddRequest()).toEqual(action);
+  });
+  it('should return success of a successful document add', () => {
+    const action = {
+      type: c.DOCUMENT_ADD_SUCCESS
+    };
+    expect(actions.documentsAddSuccess()).toEqual(action);
+  });
+  it('should fail on unsuccessful document addition', () => {
+    const action = {
+      type: c.DOCUMENT_ADD_FAILURE
+    };
+    expect(actions.documentsAddFailure()).toEqual(action);
+  });
+  it('should close document on close', () => {
+    const action = {
+      type: c.CLOSE_DOCUMENT
+    };
+    expect(actions.closeDocument()).toEqual(action);
+  });
+  it('should delete document on delete request', () => {
+    const action = {
+      type: c.DOCUMENT_DELETE_REQUEST
+    };
+    expect(actions.documentsDeleteRequest()).toEqual(action);
+  });
+  it('should delete a document on delete success', () => {
+    const expectedAction = {
+      type: c.DOCUMENT_DELETE_SUCCESS
+    };
+    expect(actions.documentsDeleteSuccess()).toEqual(expectedAction);
+  });
+  it('should fail to delete on delete failure', () => {
+    const action = {
+      type: c.DOCUMENT_DELETE_FAILURE
+    };
+    expect(actions.documentsDeleteFailure()).toEqual(action);
+  });
 
 
+});
