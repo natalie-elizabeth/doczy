08b53506e62694b08c30fc3a83ac30d872f42bfd
Rachael Njeri <rachael.njeri@andela.com>
2019-04-09 13:47:51 +0300

    ES6 + linting fixes
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- server/models/document.js
+++ server/models/document.js
@@ -1,27 +1,30 @@
 'use strict';
-module.exports = function (sequelize, DataTypes) {
-  var Document = sequelize.define('Document', {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        notEmpty: true,
-      }
+module.exports = function(sequelize, DataTypes) {
+  const Document = sequelize.define(
+    'Document',
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          notEmpty: true
+        }
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      access: DataTypes.ENUM('private', 'public', 'role')
     },
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      validate: {
-        notEmpty: true
-      }
-    },
-    access: DataTypes.ENUM('private', 'public', 'role')
-  }, {
+    {
       tableName: 'documents',
       underscored: true,
       classMethods: {
-        associate: function (models) {
+        associate: function(models) {
           // associations can be defined here
           Document.belongsTo(models.User, {
             foreignKey: 'user_id',
@@ -29,6 +32,7 @@ module.exports = function (sequelize, DataTypes) {
           });
         }
       }
-    });
+    }
+  );
   return Document;
 };
--- server/routes/users/users.controller.js
+++ server/routes/users/users.controller.js
@@ -5,17 +5,34 @@ const secretKey = process.env.SECRET_KEY || 'some secret';
 const { User, Document } = require('../../models');
 
 class UserController {
-
   static create(req, res) {
-    const { username, firstname, lastname, email, password, role_id } = req.body;
+    const {
+      username,
+      firstname,
+      lastname,
+      email,
+      password,
+      role_id
+    } = req.body;
 
     if (!username || !firstname || !lastname || !email || !password) {
-      return res.status(400).json({ message: 'Missing required field name' });
-    } else if (!(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email))) {
-      return res.status(400).json({ message: 'Please Enter A Valid Email Address' });
+      return res.status(400).json({ message: 'Missing required field' });
+    } else if (
+      !/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email)
+    ) {
+      return res
+        .status(400)
+        .json({ message: 'Please Enter A Valid Email Address' });
     }
 
-    return User.create({ username, firstname, lastname, email, password, role_id })
+    return User.create({
+      username,
+      firstname,
+      lastname,
+      email,
+      password,
+      role_id
+    })
       .then(user => res.status(201).json(user))
       .catch(error => {
         res.status(400).json(error);
@@ -31,30 +48,30 @@ class UserController {
     User.all()
       .then(users => res.status(201).send(users))
       .catch(error => res.status(400).send(error));
-
   }
 
-
   static listall(req, res) {
-    return User
-      .findAll({
-        include: [{
+    return User.findAll({
+      include: [
+        {
           model: Document,
-          as: 'documents',
-        }],
-      })
+          as: 'documents'
+        }
+      ]
+    })
       .then(users => res.status(200).json(users))
       .catch(error => res.status(404).send(error));
   }
 
   static retrieve(req, res) {
-    return User
-      .findById(req.params.id, {
-        include: [{
+    return User.findById(req.params.id, {
+      include: [
+        {
           model: Document,
           as: 'documents'
-        }]
-      })
+        }
+      ]
+    })
       .then(user => {
         if (!user) {
           res.status(404).send({
@@ -63,15 +80,16 @@ class UserController {
         }
         return res.status(200).json(user);
       })
-      .catch(error => { res.status(400).json(error); });
+      .catch(error => {
+        res.status(400).json(error);
+      });
   }
 
   static delete(req, res) {
-    return User
-      .findById(req.params.id)
+    return User.findById(req.params.id)
       .then(user => {
         if (!user) {
-          res.status(404).send({ message: "User not found" });
+          res.status(404).send({ message: 'User not found' });
         }
         return user
           .destroy()
@@ -82,49 +100,49 @@ class UserController {
   }
 
   static update(req, res) {
-    return User
-      .findById(req.params.id)
-      .then(user => {
-        if (!user) {
-          return res.status(404).send({
-            message: 'User Not Found',
-          });
-        }
-        return user
-          .update({
-            username: req.body.username || user.username,
-            email: req.body.email || user.email,
-            password: req.body.password || user.password
-
-          })
-          .then(() => res.status(200).send(user))
-          .catch((error) => res.status(400).send(error));
-      });
+    return User.findById(req.params.id).then(user => {
+      if (!user) {
+        return res.status(404).send({
+          message: 'User Not Found'
+        });
+      }
+      return user
+        .update({
+          username: req.body.username || user.username,
+          email: req.body.email || user.email,
+          password: req.body.password || user.password
+        })
+        .then(() => res.status(200).send(user))
+        .catch(error => res.status(400).send(error));
+    });
   }
 
   static login(req, res) {
-    return User
-      .findOne({
-        where: {
-          email: req.body.email
-        }
-      })
-      .then((user) => {
+    return User.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+      .then(user => {
         if (!user) {
           return res.status(403).json({
-            message: 'Invalid user',
+            message: 'Invalid user'
           });
         }
         bcrypt.compare(req.body.password, user.password, (err, matched) => {
           if (err) {
             return res.status(401).json({
-              message: 'Invalid credentials',
+              message: 'Invalid credentials'
             });
           }
           if (!matched) {
             return res.json('password or email is incorrect');
           }
-          const token = jwt.sign({ userId: user.id, roleId: user.role_id }, secretKey, { expiresIn: '24h' });
+          const token = jwt.sign(
+            { userId: user.id, roleId: user.role_id },
+            secretKey,
+            { expiresIn: '24h' }
+          );
           return res.status(200).json({
             message: 'Login Successful',
             token,
@@ -132,13 +150,12 @@ class UserController {
           });
         });
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error);
         res.status(401).send({
           message: 'invalid'
         });
       });
-
   }
   static logout(req, res) {
     res.status(200).send({
@@ -148,16 +165,17 @@ class UserController {
 
   static search(req, res) {
     if (req.query.q) {
-      return user.findAll({
-        where: {
-          $or: [
-            { firstname: { $like: `%${req.query.q}%` } },
-            { lastname: { $like: `%${req.query.q}%` } },
-            { username: { $like: `%${req.query.q}%` } },
-            { email: { $like: `%${req.query.q}%` } }
-          ]
-        }
-      })
+      return user
+        .findAll({
+          where: {
+            $or: [
+              { firstname: { $like: `%${req.query.q}%` } },
+              { lastname: { $like: `%${req.query.q}%` } },
+              { username: { $like: `%${req.query.q}%` } },
+              { email: { $like: `%${req.query.q}%` } }
+            ]
+          }
+        })
         .then(response => res.status(200).send(response))
         .catch(error => res.status(400).send(error));
     }
